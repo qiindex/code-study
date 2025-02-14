0f75c54d594c2a4d096d86e026609340724696af
yuanqiang.qi <yuanqiang.qi@shopee.com>
2025-02-14 11:38:07 +0800

    last one

---
+++ go-study/business/string_info/string_tool.go
@@ -0,0 +1,7 @@
+package string_info
+
+import "strings"
+
+func UpperName(name string) string {
+	return strings.ToUpper(name)
+}
---
+++ go-study/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	 "go-study/business/list_info"
+	 "go-study/business/string_info"
+)
+
+func main() {
+	println("222")
+	// is the demo
+	println(list_info.SortInfo(2))
+	name := string_info.UpperName("New world")
+	println(name)
+
+}
