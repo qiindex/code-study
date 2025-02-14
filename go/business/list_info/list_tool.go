cc24d469a3a1fac77ef4e4a2c3959cb5c0156e19
yuanqiang.qi <yuanqiang.qi@shopee.com>
2025-02-14 10:53:33 +0800

    第三次提交

---
+++ go/business/list_info/list_tool.go
@@ -0,0 +1,5 @@
+package listinfo
+
+func  sortInfo(a int ) int {
+	return a *a
+}
--- go/main.go
+++ go/main.go
@@ -1,6 +1,9 @@
 package main
+import "github.com/yuanqiang-go-demo-0214-v1/go/business/list_info"
 
 func main() {
 	println("222")
 	// is the demo
+
+	list_info.sortInfo(1)
 }
