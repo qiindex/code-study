d24d2ee86bba78422db631c97243c2a1ca96388e
qiindex <63623577+qiindex@users.noreply.github.com>
2025-02-23 09:30:48 +0000

    "dd"

--- go-study/main.go
+++ go-study/main.go
@@ -6,10 +6,20 @@ import (
 )
 
 func main() {
+	demo1()
+	demo2()
+}
+func demo1(){
 	println("222")
 	// is the demo
 	println(list_info.SortInfo(2))
 	name := string_info.UpperName("New world")
 	println(name)
+	var a int 
+	var  b  int = 2
+	println(a+ b)
+}
+
+func demo2(){
 
 }
