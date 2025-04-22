c60523c2723dbb78bcfaa414bd8aa8a5d4c89312
yuanqiang.qi <yuanqiang.qi@shopee.com>
2025-04-22 16:59:28 +0800

    0422-修改代码命名规则

---
+++ python_script/test_demo.py
@@ -0,0 +1,14 @@
+
+def foo():
+    print("This is foo function")
+    a = 153 /143.9
+    print(a)
+
+
+
+def main():
+    print("Hello, World!")
+    foo()
+
+if __name__ == "__main__":
+    main()
