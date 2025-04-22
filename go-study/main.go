a0dbae977c052d887d42acc150a2c0eb3cfc6ce8
yuanqiang.qi <yuanqiang.qi@shopee.com>
2025-04-22 16:33:25 +0800

    demo

--- go-study/main.go
+++ go-study/main.go
@@ -1,25 +1,26 @@
 package main
 
 import (
-	 "go-study/business/list_info"
-	 "go-study/business/string_info"
+	"go-study/business/list_info"
+	"go-study/business/string_info"
 )
 
 func main() {
 	demo1()
 	demo2()
+	//ddd
 }
-func demo1(){
+func demo1() {
 	println("222")
 	// is the demo
 	println(list_info.SortInfo(2))
 	name := string_info.UpperName("New world")
 	println(name)
-	var a int 
-	var  b  int = 2
-	println(a+ b)
+	var a int
+	var b int = 2
+	println(a + b)
 }
 
-func demo2(){
+func demo2() {
 
 }
