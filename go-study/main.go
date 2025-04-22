94897f05375d5c7bc9600d0cb5542f01f2ca82b8
yuanqiang.qi <yuanqiang.qi@shopee.com>
2025-04-22 16:50:29 +0800

    demo 代码搬迁

--- go-study/main.go
+++ go-study/main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
+	"go-study/algorithm/demo_design"
+	"go-study/algorithm/go_algo"
 	"go-study/business/list_info"
 	"go-study/business/string_info"
 )
 
-func main() {
-	demo1()
-	demo2()
-	//ddd
-}
 func demo1() {
 	println("222")
 	// is the demo
@@ -21,6 +18,12 @@ func demo1() {
 	println(a + b)
 }
 
-func demo2() {
-
+func main() {
+	// demo
+	demo1()
+	demo_design.Demo()
+	go_algo.CompareString()
+	//go_algo.Main1()
+	//LRU缓存_手写版_加锁类型.NewLruCache(2)
+	//lru_cache.Constructor(2)
 }
