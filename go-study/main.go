531a8ab3daf161be4ae1e4850adcd2ce8d015b9a
yuanqiang.qi <yuanqiang.qi@shopee.com>
2025-04-22 16:51:10 +0800

    demo 代码搬迁

--- go-study/main.go
+++ go-study/main.go
@@ -3,24 +3,10 @@ package main
 import (
 	"go-study/algorithm/demo_design"
 	"go-study/algorithm/go_algo"
-	"go-study/business/list_info"
-	"go-study/business/string_info"
 )
-
-func demo1() {
-	println("222")
-	// is the demo
-	println(list_info.SortInfo(2))
-	name := string_info.UpperName("New world")
-	println(name)
-	var a int
-	var b int = 2
-	println(a + b)
-}
 
 func main() {
 	// demo
-	demo1()
 	demo_design.Demo()
 	go_algo.CompareString()
 	//go_algo.Main1()
