2f21400c2ad37cdea4ffb6172174964d82ab0fbd
yuanqiang.qi <yuanqiang.qi@shopee.com>
2025-04-28 11:52:49 +0800

    2025-04-22:编辑距离

--- go-project/go.mod
+++ go-project/go.mod
@@ -1,4 +1,4 @@
-module go-study
+module go-project
 
 go 1.16
 
--- go-project/main.go
+++ go-project/main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"fmt"
-
-	"go-study/algorithm/demo_design"
-	"go-study/business/int_info"
-	"go-study/business/link_info"
+	"go-project/algorithm/demo_design"
+	"go-project/business/link_info"
 )
 
-func main1() {
+func main() {
 	// demo
 	demo_design.Demo()
 	//
@@ -16,18 +13,4 @@ func main1() {
 	link_info.PrintList(linkList)
 	/*reverseLink := link_info.RemoveNthFromEnd(linkList, 2)
 	link_info.PrintList(reverseLink)*/
-}
-func main() {
-	// 示例输入
-	nums := []int{-1, 0, 1, 2, -1, -4}
-
-	// 调用 threeSum 函数
-	result := int_info.ThreeSum(nums)
-
-	// 打印结果
-	fmt.Println("输入数组:", nums)
-	fmt.Println("所有和为 0 的不重复三元组:")
-	for _, triplet := range result {
-		fmt.Println(triplet)
-	}
 }
