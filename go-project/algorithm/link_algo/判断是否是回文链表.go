391538d2581cbd88ee762cfdb5216eeb3a8b683e
yuanqiang.qi <yuanqiang.qi@shopee.com>
2025-04-30 10:30:20 +0800

    2025-04-28:回文链表，可以用两种方法：一种是转成数组，用下标判断； 一种是翻转链表的后面1/2不分，然后循环1/2的原始head和翻转后的reverse的链表比较；

--- go-project/algorithm/link_algo/判断是否是回文链表.go
+++ go-project/algorithm/link_algo/判断是否是回文链表.go
@@ -2,8 +2,80 @@ package link_algo
 
 /*
 给你一个单链表的头节点 head ，请你判断该链表是否为回文链表。如果是，返回 true ；否则，返回 false 。
+输入：head = [1,2,2,1]
+输出：true
 
+*/
+// 思路，用数据先承接链表的数字，然后判断数组，估计不行，空间复杂度O(N)了；（时间复杂度O(n)）
+func IsPalindromeV1(head *ListNode) bool {
+	list := make([]int, 0)
+	for head != nil {
+		list = append(list, head.Val)
+		head = head.Next
+	}
+	//fmt.Println(list)
+	for i := 0; i < (len(list) / 2); i++ {
+		if list[i] != list[len(list)-i-1] {
+			return false
+		}
+	}
+	return true
 
+}
 
+// 最优解：翻转链表，然后比较，双指针，O（1）的空间复杂度；
+func IsPalindrome(head *ListNode) bool {
+	//[1,2,3,2,1]
+	//[1,2,2,1]
+	//翻转n/2之后的链表，然后连接上
+	count := 0
+	dummy := head
+	for dummy != nil {
+		dummy = dummy.Next
+		count++
+	} // 1 2 3 1
+	pre := &ListNode{Next: head}
+	var reLink *ListNode
+	for i := 0; i < count/2; i++ {
+		pre = pre.Next
+		reLink = pre.Next
+	}
+
+	// 翻转relink的首尾，返回新的首
+	newHead := reverseListNode(reLink)
+	pre.Next = newHead
+	//  1. 2  1 3
+	if pre != nil {
+		pre = pre.Next
+	}
+	for i := 0; i < count/2; i++ {
+		if pre != nil && head.Val != pre.Val {
+			return false
+		}
+		if pre != nil {
+			pre = pre.Next
+		}
+		head = head.Next
+	}
+	return true
+
+}
+func reverseListNode(head *ListNode) *ListNode {
+	var pre *ListNode
+	for head != nil {
+		next := head.Next
+		head.Next = pre
+		pre = head
+		head = next
+	}
+	return pre
+}
+
+/*
+func main() {
+	// demo
+	listLink := link_algo.CreateList([]int{1,2,2,1})
+	fmt.Println(link_algo.IsPalindrome(listLink))
+}
 
 */
--- go-project/main.go
+++ go-project/main.go
@@ -2,52 +2,12 @@ package main
 
 import (
 	"fmt"
+
+	"go-project/algorithm/link_algo"
 )
 
 func main() {
 	// demo
-
-	fmt.Println(CountNumberNV2(3))
-}
-
-// 跳台阶问题，一次跳一个，一次跳2个，跳N个台阶需要多少步？
-// 思路：（假设n>0,如果n=0，还需要增加逻辑直接返回0）
-func CountNumber(n int) int {
-	dp := make([]int, n+1)
-	dp[0] = 1 // 如果这里不写dp[0] =1,则需要判断n=0时，直接return 1
-	dp[1] = 1
-	dp[2] = 2
-	if n <= 2 {
-		return dp[n]
-	}
-
-	//dp[3] = 3  推导
-	//dp[4] = 5
-	for i := 3; i <= n; i++ {
-		dp[i] = dp[i-1] + dp[i-2]
-	}
-
-	return dp[n]
-
-}
-
-// 跳台阶问题，如果一次跳N个台阶?，一种是数学推导出来结果是2的（n-1)次方，一种是用dp，dp计算前面的累加和；建议第一种；
-func CountNumberN(n int) int {
-	return 1 << (n - 1)
-}
-
-// 使用dp计算一次可以跳N个台阶
-func CountNumberNV2(n int) int {
-	dp := make([]int, n+1)
-	dp[0] = 1
-	dp[1] = 1
-	dp[2] = 2
-	//dp[3] = 4 推导
-	//dp[4] = 8
-	for i := 3; i <= n; i++ {
-		for j := 0; j < i; j++ {
-			dp[i] += dp[j]
-		}
-	}
-	return dp[n]
+	listLink := link_algo.CreateList([]int{1, 2, 2, 1})
+	fmt.Println(link_algo.IsPalindrome(listLink))
 }
