a5a489dca6cf0e360af9be533e31302d8eaeac3a
yuanqiang.qi <yuanqiang.qi@shopee.com>
2025-04-30 10:35:55 +0800

    2025-04-28:回文链表，可以用两种方法：一种是转成数组，用下标判断； 一种是翻转链表的后面1/2不分，然后循环1/2的原始head和翻转后的reverse的链表比较；

--- go-project/main.go
+++ go-project/main.go
@@ -8,6 +8,20 @@ import (
 
 func main() {
 	// demo
-	listLink := link_algo.CreateList([]int{1, 2, 2, 1})
+	listLink := CreateListNode([]int{1, 2, 2, 1})
 	fmt.Println(link_algo.IsPalindrome(listLink))
+}
+
+func CreateListNode(list []int) *link_algo.ListNode {
+	head := &link_algo.ListNode{
+		Val:  list[0],
+		Next: nil,
+	}
+	current := head
+	for i := 1; i < len(list); i++ {
+		temp := &link_algo.ListNode{Val: list[i]}
+		current.Next = temp
+		current = current.Next
+	}
+	return head
 }
